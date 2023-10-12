41e4ee99bb5e056bd50f483d86038fc7b3a2f887
taemin817 <tmpark11@naver.com>
2023-10-13 03:26:24 +0900

    ex02 시작

--- ex01/src/main/java/org/zerock/controller/SampleController.java
+++ ex01/src/main/java/org/zerock/controller/SampleController.java
@@ -5,12 +5,14 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -43,22 +45,23 @@ public class SampleController {
 		log.info("basic getr only get......");
 	}
 	
-	@GetMapping("/ex01")
+	@GetMapping("/ex01")	// sample/ex01?name=어쩌구&age=저쩌구
 	public String ex01(SampleDTO dto) {
-		log.info(""+dto);
+		log.info(""+dto);	// name : 어쩌구, age : 저쩌구  출력
 		
-		return "ex01";
+		return "ex01";	
 	}
 	
-	@GetMapping("/ex02")
-	public String ex02(@RequestParam("name")String name,@RequestParam("age") int age) {
-		log.info("name : "+name);
+	@GetMapping("/ex02")// 파라미터로사용된 변수의 이름과 전달되는 파라미터의 이름이 다른 경우 사용
+						//  /sample/ex02?name=어쩌구&age=저쩌구
+	public String ex02(@RequestParam("name")String naming,@RequestParam("age") int age) {
+		log.info("name : "+naming);
 		log.info("age : "+age);
 		
 		return "ex02";
 	}
 	
-	@GetMapping("/ex02List")
+	@GetMapping("/ex02List")				// ids로 여러번 입력 가능하도록 ArrayList 사용(배열도 동일)
 	public String ex02List(@RequestParam("ids")ArrayList<String> ids) {
 		
 		log.info("ids : "+ids);
@@ -73,14 +76,14 @@ public class SampleController {
 		return "ex02Array";
 	}
 	
-	@GetMapping("/ex02Bean")
+	@GetMapping("/ex02Bean")	// sample/ex02Bean?list[0].name=aaa&lis[1].name=bbb처럼 []형식으로 가능
 	public String ex02Bean(SampleDTOList list) {
 		log.info("list dtos : "+list);
-		
+			
 		return "ex02Bean";
 	}
 	
-
+	// @InitBinder TodoDTO에서 @DateTimeFormat사용하면 생략가능
 	public void initBinder(WebDataBinder binder) {
 		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
 		binder.registerCustomEditor(
@@ -95,7 +98,7 @@ public class SampleController {
 	}
 	
 	@GetMapping("/ex04")
-	public String ex04(SampleDTO dto, @ModelAttribute("page") int page) {
+	public String ex04(SampleDTO dto, @ModelAttribute("page") int page) {	// 기본형타입을 강제로 모델에 주입
 		log.info("dto : "+dto);
 		log.info("page : "+page);
 		
@@ -103,14 +106,14 @@ public class SampleController {
 	}
 	
 	@GetMapping("/ex05")
-	public void ex05() {
+	public void ex05() {	// void라서 return없고 ex05파일 찾아감 =>현재는 ex05.jsp가 없으므로 404
 		log.info("/ex05.....");
 	}
 	
-	@GetMapping("/ex06")
+	@GetMapping("/ex06")	// dto 반환해야하므로 @Responsebody를 SampleDTO앞에 달아줌
 	public @ResponseBody SampleDTO ex06() {
 		log.info("/ex06.......");
-		SampleDTO dto = new SampleDTO();
+		SampleDTO dto = new SampleDTO();	// 객체 타입 => pom.xml에 jackson-databind를 추가해줌
 		dto.setAge(10);
 		dto.setName("taemin");
 		
@@ -126,8 +129,8 @@ public class SampleController {
 		
 		HttpHeaders header = new HttpHeaders();
 		header.add("Content-Type", "application/json;charset=UTF-8");
-		
-		return new ResponseEntity<>(msg, header, HttpStatus.OK);
+				// HttpServletRequest,Response대신 ResponseEntity 사용
+		return new ResponseEntity<>(msg, header, HttpStatus.OK);	// name : 홍길동, 200 OK 반환
 	}
 	
 	@GetMapping("/exUpload")
@@ -137,6 +140,7 @@ public class SampleController {
 	
 	@PostMapping("/exUploadPost")
 	public void ExUploadPost(ArrayList<MultipartFile> files) {
+							// 전달되는 파라미터가 동일한 이름으로 여러개 존재하면 배열로 처리가능 => 파라미터를 MultipartFile로 작성
 		files.forEach(file->{
 			log.info("-------------------------");
 			log.info("name : "+file.getOriginalFilename());
--- ex01/src/main/java/org/zerock/exception/CommonExceptionAdvice.java
+++ ex01/src/main/java/org/zerock/exception/CommonExceptionAdvice.java
@@ -9,11 +9,11 @@ import org.springframework.web.servlet.NoHandlerFoundException;
 
 import lombok.extern.log4j.Log4j;
 
-@ControllerAdvice
+@ControllerAdvice	// AOP를 이용해 공통 관심사는 분리하자는 개념, 공통적인 예외사항에 대해서 별도로 @ControllerAdvice를 이용해 분리
 @Log4j
 public class CommonExceptionAdvice {
 
-	@ExceptionHandler(Exception.class)
+	@ExceptionHandler(Exception.class)	// 괄호 안에 있는 예외타입만 처리함 => Exception.class니까 except()만을 이용해 처리 가능
 	public String except(Exception ex, Model model) {
 		
 		log.error("Exception....." + ex.getMessage());
@@ -24,7 +24,7 @@ public class CommonExceptionAdvice {
 	}
 	
 	@ExceptionHandler(NoHandlerFoundException.class)
-	@ResponseStatus(HttpStatus.NOT_FOUND)
+	@ResponseStatus(HttpStatus.NOT_FOUND)	// 존재하지않는 주소일때
 	public String handle404(NoHandlerFoundException ex) {
 		return "custom404";
 	}
